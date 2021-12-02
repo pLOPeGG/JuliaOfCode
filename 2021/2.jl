7203f0dc3ff0bd238bf9df22c13b4db6c637febc
Thibault Douzon <douzont@gmail.com>
2021-12-03 00:56:57 +0100

    y2021d2 assignment in match macro

--- 2021/2.jl
+++ 2021/2.jl
@@ -10,10 +10,10 @@ const data = parse_line.(readlines())
 function solve_1()
     pos, depth = 0, 0
     for (mv, i) in data
-        pos, depth = @match (mv, i) begin
-            ("forward", i) => (pos + i, depth)
-            ("up", i) => (pos, depth - i)
-            ("down", i) => (pos, depth + i)
+        @match (mv, i) begin
+            ("forward", i) => (pos += i)
+            ("up", i) => (depth -= i)
+            ("down", i) => (depth += i)
         end
     end
     pos * depth
@@ -22,10 +22,10 @@ end
 function solve_2()
     pos, depth, aim = 0, 0, 0
     for (mv, i) in data
-        pos, depth, aim = @match (mv, i) begin
-            ("forward", i) => (pos + i, depth + aim * i, aim)
-            ("up", i) => (pos, depth, aim - i)
-            ("down", i) => (pos, depth, aim + i)
+        @match (mv, i) begin
+            ("forward", i) => (pos += i; depth += aim * i)
+            ("up", i) => (aim -= i)
+            ("down", i) => (aim += i)
         end
     end
     pos * depth
