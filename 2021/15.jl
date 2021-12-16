9d77b7e0ac296a1dcbf743cf7ed25f4f4c7b3b38
Thibault DOUZON <thibault.douzon@esker.fr>
2021-12-16 11:20:15 +0100

    y2021d15

--- 2021/15.jl
+++ 2021/15.jl
@@ -2,23 +2,32 @@ using DataStructures
 
 const data = parse.(Int, reduce(hcat, collect.(readlines())))
 
-neighbours(pos) =
+neighbours(pos; expand = 1) =
     filter([
         CartesianIndex(pos.I .- n)
         for n ∈ ((-1, 0), (1, 0), (0, -1), (0, 1))
     ]) do I
-        1 <= I.I[1] <= size(data, 1) && 1 <= I.I[2] <= size(data, 2)
+        1 <= I.I[1] <= size(data, 1) * expand && 1 <= I.I[2] <= size(data, 2) * expand
     end
 
-function dijkstra(start, finish)
+divmod1(x, y) = (div(x, y) - (rem(x, y) == 0 ? 1 : 0), mod1(x, y))
+
+function get_val(data, pos::CartesianIndex)
+    qx, rx = divmod1(pos[1], size(data, 1))
+    qy, ry = divmod1(pos[2], size(data, 2))
+
+    reduced_pos = CartesianIndex(rx, ry)
+    mod1(data[reduced_pos] + qx + qy, 9)
+end
+
+function dijkstra(start, finish; expand = 1)
     q = PriorityQueue{CartesianIndex,Int}(Base.Order.Forward)
     seen = Set{CartesianIndex}()
 
-    enqueue!(q, start => data[start])
+    enqueue!(q, start => 0)
 
     while !isempty(q)
         pos, d = dequeue_pair!(q)
-        @show pos, d
         if pos ∈ seen
             continue
         else
@@ -27,12 +36,12 @@ function dijkstra(start, finish)
         if pos == finish
             return d
         end
-        for nei in neighbours(pos)
+        for nei in neighbours(pos; expand)
             if nei ∉ seen
                 if nei ∉ keys(q)
-                    enqueue!(q, nei => d + data[nei])
+                    enqueue!(q, nei => d + get_val(data, nei))
                 else
-                    q[nei] = min(q[nei], d + data[nei])
+                    q[nei] = min(q[nei], d + get_val(data, nei))
                 end
             end
         end
@@ -40,4 +49,8 @@ function dijkstra(start, finish)
     return nothing
 end
 
+# Part One
 dijkstra(CartesianIndex(1, 1), CartesianIndex(size(data)...)) |> println
+
+# Part Two
+dijkstra(CartesianIndex(1, 1), CartesianIndex(size(data)...) * 5; expand = 5) |> println
