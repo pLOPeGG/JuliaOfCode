001014e1d2f283dc67a496f46c8f4c12a093c61f
Thibault Douzon <douzont@gmail.com>
2023-12-02 19:05:37 +0100

    tidying up code refactor

--- 2023/2.jl
+++ 2023/2.jl
@@ -1,42 +1,54 @@
+using MLStyle
+
 bag = Dict(
     "red" => 12,
     "green" => 13,
-    "blue" => 14)
+    "blue" => 14
+)
 
-function parseline(line)
-    _, content = split(line, ": ")
+const Draw = Dict{String,Int}
+
+struct Qame
+    id::Int
+    draws::Vector{Draw}
+end
+
+
+function parseline(line)::Game
+    str_id, content = split(line, ": ")
+    game_id = parse(Int, split(str_id)[end])
     draws = split.(split(content, "; "), ", ")
 
-    games = []
-    for game in draws
-        picked_colours = Dict{String,Int}()
-        for hand in game
+    game_draws = Draw[]
+    for draw in draws
+        picked_colours = Draw()
+        for hand in draw
             n, colour = split(hand)
             nn = parse(Int, n)
             picked_colours[colour] = nn
         end
-        push!(games, picked_colours)
+        push!(game_draws, picked_colours)
     end
-    games
+    Game(game_id, game_draws)
 end
 
 data = readlines() .|> parseline
 
 function solve1()
-    sum(enumerate(data)) do (i, game)
-        all(game) do hands
+    sum(data) do game
+        all(game.draws) do hands
             all(hands) do (colour, n)
                 bag[colour] >= n
             end
 
-        end ? i : 0
+        end ? game.id : 0
     end
 end
 
 function solve2()
     sum(data) do game
         mini_bag = Dict(colour => 0 for colour in keys(bag))
-        for hand in game
+        for hand in game.draws
             for (c, n) in hand
                 mini_bag[c] = max(n, mini_bag[c])
             end
